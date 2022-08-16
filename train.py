3a67ef8ee8a4ca3193b003b339567b8efbde1829
YounesAEO <y.aboueleomoum09@gmail.com>
2022-08-16 15:34:40 +0200

    add plotting functions during training to get error and loss and error curves

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ from torchvision import transforms
 import argparse
 import dataset
 import time
+import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='PyTorch CSRNet')
 
@@ -40,6 +41,7 @@ def main():
     best_prec1 = 1e6
     
     args = parser.parse_args()
+    args.original_lr = 1e-7
     args.lr = 1e-7
     args.batch_size    = 1
     args.momentum      = 0.95
@@ -47,12 +49,13 @@ def main():
     args.start_epoch   = 0
     args.epochs = 400
     args.workers = 4
+    args.steps = [-1,1,100,150]
+    args.scales = [1,1,1,1]
     args.seed = time.time()
     args.print_freq = 30
     
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
     torch.cuda.manual_seed(args.seed)
-    
     model = CSRNet()
     
     model = model.cuda()
@@ -78,11 +81,13 @@ def main():
         else:
             print("=> no checkpoint found at '{}'".format(args.pre))
             
+    loss_values = []
+    mae_values = []
     for epoch in range(args.start_epoch, args.epochs):
-        
-        train(train_list, model, criterion, optimizer, epoch)
+        adjust_learning_rate(optimizer, epoch)
+
+        loss_avg = train(train_list, model, criterion, optimizer, epoch)
         prec1 = validate(val_list, model, criterion)
-        
         is_best = prec1 < best_prec1
         best_prec1 = min(prec1, best_prec1)
         print(' * best MAE {mae:.3f} '
@@ -95,12 +100,41 @@ def main():
             'optimizer' : optimizer.state_dict(),
         }, is_best,args.task)
 
+        loss_values.append(loss_avg)
+        mae_values.append(prec1)
+        if epoch % 10 == 0:
+            print('saving current loss curve..')
+            plt.figure()
+            plt.plot(loss_values)
+            plt.xlabel('epochs')
+            plt.ylabel('loss')
+            plt.savefig('loss_curve.png')
+            print('saving current mae curve..')
+            plt.figure()
+            plt.plot(mae_values)
+            plt.xlabel('epochs')
+            plt.ylabel('average MAE')
+            plt.savefig('mae_curve.png')
+            print('done.')
+
+    ## plot latest loss values
+    plt.figure()
+    plt.plot(loss_values)
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.savefig('loss_curve_last.png')
+    ## plot latest mae values
+    plt.figure()
+    plt.plot(mae_values)
+    plt.xlabel('epochs')
+    plt.ylabel('average MAE')
+    plt.savefig('mae_curve_latest')
+
 def train(train_list, model, criterion, optimizer, epoch):
     
     losses = AverageMeter()
     batch_time = AverageMeter()
     data_time = AverageMeter()
-    
     train_loader = torch.utils.data.DataLoader(
         dataset.listDataset(train_list,
                        shuffle=True,
@@ -117,21 +151,22 @@ def train(train_list, model, criterion, optimizer, epoch):
     
     model.train()
     end = time.time()
-    
+
     for i,(img, target)in enumerate(train_loader):
         data_time.update(time.time() - end)
         
         forward_start = time.perf_counter()
         img = img.cuda()
         img = Variable(img)
-        output = model(img)
         forward_end = time.perf_counter()
-        
-        target_start = time.perf_counter()
+        output = model(img)
+
+        target_start = time.perf_counter()        
         target = target.type(torch.FloatTensor).unsqueeze(0).cuda()
         target = Variable(target)
         target_end = time.perf_counter()
 
+        
         loss_start = time.perf_counter()
         loss = criterion(output, target)
         losses.update(loss.item(), img.size(0))
@@ -141,9 +176,8 @@ def train(train_list, model, criterion, optimizer, epoch):
         backward_start = time.perf_counter()
         loss.backward()
         optimizer.step()    
-        backward_end = time.perf_counter()
-
         batch_time.update(time.time() - end)
+        backward_end = time.perf_counter()
         end = time.time()
         
         if i % args.print_freq == 0:
@@ -160,7 +194,8 @@ def train(train_list, model, criterion, optimizer, epoch):
                    data_time=data_time, loss=losses, forward_time=forward_end-forward_start,
                    target_time=target_end-target_start, loss_time=loss_end-loss_start,
                    backward_time=backward_end-backward_start))
-    
+    return losses.avg
+
 def validate(val_list, model, criterion):
     print ('begin test')
     test_loader = torch.utils.data.DataLoader(
@@ -188,7 +223,27 @@ def validate(val_list, model, criterion):
               .format(mae=mae))
 
     return mae    
+
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    
+    
+    args.lr = args.original_lr
+    
+    for i in range(len(args.steps)):
         
+        scale = args.scales[i] if i < len(args.scales) else 1
+        
+        
+        if epoch >= args.steps[i]:
+            args.lr = args.lr * scale
+            if epoch == args.steps[i]:
+                break
+        else:
+            break
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = args.lr
+
 class AverageMeter(object):
     """Computes and stores the average and current value"""
     def __init__(self):
